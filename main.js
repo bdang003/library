9127f868f6e900b92b93afde526340f146e0b639
bdang003 <bdang003@gmail.com>
2021-07-18 00:17:49 -0700

    modified addBookToLibrary() to use form instead of prompt

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-for(let i=0; i<5; i++){ //creates temp list of books for library
+for(let i=0; i<1; i++){ //creates temp list of books for library
     myLibrary.push(new Book(`book${i}`, `book${i}`));
 } //remove when you are ready to do without it
 
@@ -8,20 +8,12 @@ initializePage();
 function initializePage(){
     const main = document.querySelector('#main');
     const library = document.querySelector('#library');
-    createAddButton();  
     for(let i=0; i<myLibrary.length; i++){
         createBookDiv(myLibrary[i]);
     }
-}
-
-function createAddButton(){
-    let addButton = document.createElement('button');
-    addButton.innerHTML= 'Add Book';
-    addButton.type='submit';
-    addButton.onclick = function (){
-        addBookToLibrary();
-    }
-    main.appendChild(addButton); //attaches addButton to main
+    const form = document.querySelector('#form');
+    console.log(form);
+    form.addEventListener('submit', addBookToLibrary);
 }
 
 function Book(title, author){
@@ -29,12 +21,13 @@ function Book(title, author){
     this.author= author
 }
 
-function addBookToLibrary(){ //uses window.prompt to add new book
-    let title = prompt('title', 'default');
-    let author = prompt('author', 'default');
-    let newBook = new Book(title, author);
+function addBookToLibrary(e){
+    e.preventDefault();
+    let title = form.elements['title'].value;
+    let author = form.elements['author'].value;
+    let newBook = new Book(title,author);
     myLibrary.push(newBook);
-    createBookDiv(newBook); //create div with details
+    createBookDiv(newBook);
 }
 
 function createBookDiv(book){
